e7be554e1c4f9dd2d1226d27637adb51d831a5f3
mihailegorin <mihail.egorin.14@gmail.com>
2023-06-30 17:04:10 +0300

    fix

--- infrastructure/server-launch-template.tf
+++ infrastructure/server-launch-template.tf
@@ -8,7 +8,7 @@ resource "aws_launch_template" "web" {
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.web.id]
   iam_instance_profile   = aws_iam_instance_profile.ec2_profile.name
-  user_data              = templatefile("app_deployment/user_data.sh.tpl", {
+  user_data              = templatefile("infrastructure/user_data.sh.tpl", {
     artifact_bucket     = var.bucket_name,
     application_version = var.application_version
   })
--- src/main/java/mihailegorin/app/springbootwebapp/SimpleController.java
+++ src/main/java/mihailegorin/app/springbootwebapp/SimpleController.java
@@ -16,7 +16,6 @@ public class SimpleController {
 
     @GetMapping(value = {"/"})
     public ResponseEntity<GetServerStatusResponse> getServerStatus() {
-
         final var client = WebClient.create("http://169.254.169.254");
         final var ip = client.get()
                 .uri("/latest/meta-data/local-ipv4")
