0966c3430ee4f5b310bc7df6db93849f1e2ee5e6
mihailegorin <mihail.egorin.14@gmail.com>
2023-07-03 21:57:44 +0300

    V1 check deploy

--- src/main/java/mihailegorin/app/springbootwebapp/SimpleController.java
+++ src/main/java/mihailegorin/app/springbootwebapp/SimpleController.java
@@ -28,7 +28,7 @@ public class SimpleController {
         final var response = GetServerStatusResponse.builder()
                 .serverIp(ip)
                 .serverTime(new Date().toString())
-                .message("Server is working! Have a nice day :)")
+                .message("Server is working! Have a nice day :)     V-1")
                 .build();
 
         return ResponseEntity.ok(response);
