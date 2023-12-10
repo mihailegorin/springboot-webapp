d50a248081cf0f29997401ba15d8561b1e7d548b
mihailegorin <mihail.egorin.14@gmail.com>
2023-12-10 19:02:43 +0200

    commit

--- src/main/java/mihailegorin/app/springbootwebapp/SimpleSpringBootApplication.java
+++ src/main/java/mihailegorin/app/springbootwebapp/SimpleSpringBootApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SimpleSpringBootApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(SimpleSpringBootApplication.class, args);
     }
 
